8e1dbe317dbd0eebb69ed9871a48bd47f454d57e
Chalithya Sangeeth <chalithyaofficial@gmail.com>
2021-08-05 19:15:16 +0530

    successfully connected to the MSSQL db

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
-# import mysql.connector
-import sys
-import pyodbc 
+import pymssql
+
+################## Main Function ##############################
+def main():
+
+    # print('This is '+ __name__)
+    
+    conn = pymssql.connect(server='FLOKI', user='', password='', database='testDB')
+
+ 
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM testDB.dbo.Take')
+
+    for row in cursor:
+        print(row)
 
 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=FLOKI;'
-                      'Database=testDB;'
-                      'Trusted_Connection=yes;')
 
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM testDB.dbo.Take')
-
-for row in cursor:
-    print(row)
-
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="newuser",
-#     passwd=""
-# )
-
-
-# mycursor = db.cursor()
-
-# mycursor.execute("CREATE DATABASE pythonDB")
+if __name__ == '__main__':
+    main()
