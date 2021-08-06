790f0bf6d424dc3ddac19d72cba6a1178f9be91c
Chalithya Sangeeth <chalithyaofficial@gmail.com>
2021-08-06 12:29:36 +0530

    added argparses into app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import pymssql
+import argparse
 
-################## Main Function ##############################
-def main():
 
-    # print('This is '+ __name__)
-    
+
+def parse_args():
+    parser  =  argparse.ArgumentParser()
+    parser.add_argument('name', type=str, help='The table name')
+    args = parser.parse_args()
+    return args
+
+
+
+def db_connection():   
     conn = pymssql.connect(server='FLOKI', user='', password='', database='testDB')
+    return conn
 
- 
 
+
+def select_all(conn, tbl_name: str):
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM testDB.dbo.Take')
-
+    cursor.execute('SELECT * FROM testDB.dbo.{}'.format(tbl_name))
     for row in cursor:
         print(row)
+
+
+
+
+def main():
+    conn = db_connection()
+    arg_values = parse_args()
+    select_all(conn, arg_values.name )
+    
 
 
 
